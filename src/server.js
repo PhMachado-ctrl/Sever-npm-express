7fb5183b06b61a8388e9a3f21d783be6e8d2c680
Pedro Henrique Machado Bastos <phmachado@gmail.com.br>
2021-08-17 15:30:25 -0300

    Controller

--- src/server.js
+++ src/server.js
@@ -4,5 +4,11 @@ const express = require('express')
 //Cria a aplicação
 const app = express();
 
+app.get('/produtos', (req, res)=>{
+    console.log('Servidor requisitado');
+
+    return res.send('Resposta do Servidor !');
+});
+
 //escuta a porta
 app.listen(3334, ()=>console.log('Servidor ON - Rodando'))
